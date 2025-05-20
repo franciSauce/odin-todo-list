100c2690d7a1a5801299f5ffaa5cecb1aa962798
franciSauce <careyfrancisowino@gmail.com>
2025-05-20 09:52:44 +0300

    Add Todo(), Project() & ProjectFolder()

---
+++ src/factory-functions.js
@@ -0,0 +1,21 @@
+import { format, parseISO } from 'date-fns';
+
+function Todo(task, date) {
+    const dateCreated = new Date();
+    const formattedDate = format(parseISO(date), "dd/MM/yyyy");
+    let taskCompleted = false;
+    let priority = false;
+    return { task, formattedDate, dateCreated, taskCompleted, priority };
+}
+
+function Project(name, description) {
+    let todos = [];
+    return { name, description, todos };
+}
+
+function ProjectFolder(name) {
+    let projects = [];
+    return { name, projects };
+}
+
+export { Todo, Project, ProjectFolder };
