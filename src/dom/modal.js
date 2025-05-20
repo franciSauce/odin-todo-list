49cc3f2506aa20b554f661890cd661063ca8a45d
franciSauce <careyfrancisowino@gmail.com>
2025-05-20 13:12:19 +0300

    Add displayEditTodoModalPriority() & removeTodoModal()

--- src/dom/modal.js
+++ src/dom/modal.js
@@ -119,3 +119,36 @@ function displayEditTodoModalUpcoming(e) {
     modal.classList.add("show");
 }
 
+function displayEditTodoModalPriority(e) {
+    const modal = document.querySelector(".todo-modal");
+    const modalButton = document.querySelector(".td-button");
+    const taskName = document.querySelector("#task-name");
+    const dueDate = document.querySelector("#due-date");
+    const pfIndex = e.target.dataset.pfIndex;
+    const pIndex = e.target.dataset.pIndex;
+    const tdIndex = e.target.dataset.tdIndex;
+    taskName.value = projectFolders[pfIndex].projects[pIndex].todos[tdIndex].task;
+    dueDate.value = format(parse(projectFolders[pfIndex].projects[pIndex].todos[tdIndex].formattedDate, "dd/MM/yyyy", new Date()), "yyyy-MM-dd");
+    if (projectFolders[pfIndex].projects[pIndex].todos[tdIndex].priority) priority.checked = true;
+    else priority.checked = false;
+    modalButton.setAttribute("data-pf-index", pfIndex);
+    modalButton.setAttribute("data-p-index", pIndex);
+    modalButton.setAttribute("data-td-index", tdIndex);
+    modalButton.addEventListener("click", editTodoPriority);
+    modal.classList.add("show");
+}
+
+function removeTodoModal() {
+    const modal = document.querySelector(".todo-modal");
+    const modalButton = document.querySelector(".td-button");
+    modalButton.removeEventListener("click", addTodo);
+    modalButton.removeEventListener("click", editTodo);
+    modalButton.removeEventListener("click", editTodoToday);
+    modalButton.removeEventListener("click", editTodoUpcoming);
+    modalButton.removeEventListener("click", editTodoPriority);
+    modalButton.removeAttribute("data-pf-index");
+    modalButton.removeAttribute("data-p-index");
+    modalButton.removeAttribute("data-td-index");
+    modal.classList.remove("show");
+}
+
