d7e78fe38632495c9aa6b75edb55425a7d305c47
franciSauce <careyfrancisowino@gmail.com>
2025-05-20 13:17:38 +0300

    Setup the imports

---
+++ src/dom/projects.js
@@ -0,0 +1,12 @@
+import { displayEditProjectModal, displayEditTodoModal, displayProjectModal, displayTodoModal } from "./modal";
+import { removeProject, selectProject } from "../controllers/projects";
+import { checkTodo, removeTodo } from "../controllers/todos";
+import { projectFolders } from "..";
+import { sortTodosByChecked } from "../controllers/todos";
+import deleteIcon from "../icons/delete.svg";
+import editIcon from "../icons/edit.svg";
+import addOrangeIcon from "../icons/add.svg";
+import addWhiteIcon from "../icons/add-white.svg";
+import priorityIcon from "../icons/star.svg";
+
+
