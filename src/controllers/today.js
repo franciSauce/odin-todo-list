b2c691b3e12ebe1f6e7f0cc1b5140ad042d4242d
franciSauce <careyfrancisowino@gmail.com>
2025-05-21 11:42:55 +0300

    Add editTodoToday()

--- src/controllers/today.js
+++ src/controllers/today.js
@@ -4,3 +4,23 @@ import { removeTodoModal } from "../dom/modal";
 import { displayToday } from "../dom/links";
 import { displayFolders } from "../dom/projects";
 
+function editTodoToday(e) {
+    const taskName = document.querySelector("#task-name");
+    const dueDate = document.querySelector("#due-date");
+    const priority = document.querySelector("#priority");
+    const task = taskName.value;
+    const date = dueDate.value;
+    const editedTodo = Todo(task, date);
+    if (priority.checked) {
+        editedTodo.priority = true;
+    } else {
+        editedTodo.priority = false;
+    }
+    const pfIndex = e.target.dataset.pfIndex;
+    const pIndex = e.target.dataset.pIndex;
+    const tdIndex = e.target.dataset.tdIndex;
+    projectFolders[pfIndex].projects[pIndex].todos.splice(tdIndex, 1, editedTodo);
+    removeTodoModal();
+    displayToday();
+}
+
