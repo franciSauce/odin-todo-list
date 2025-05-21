5a8347ba63c6e787f7060e366cef09f8171b6f6b
franciSauce <careyfrancisowino@gmail.com>
2025-05-21 11:06:32 +0300

    Add editTodoPriority(), removeTodoPriority() & checkTodoPriority()

--- src/controllers/priority.js
+++ src/controllers/priority.js
@@ -3,4 +3,42 @@ import { Todo } from "../factory-functions";
 import { removeTodoModal } from "../dom/modal";
 import { displayPriority } from "../dom/links";
 import { displayFolders } from "../dom/projects";
+import { tr } from "date-fns/locale";
 
+function editTodoPriority(e) {
+    const taskName = document.querySelector("#task-name");
+    const dueDate = document.querySelector("#due-date");
+    const priority = document.querySelector("#priority");
+    const task = taskName.value;
+    const date = dueDate.value;
+    const editedTodo = Todo(task, date);
+    if (priority.checked) {
+        editedTodo.priority = true;
+    } else {
+        editedTodo.priority = false;
+    }
+    const pfIndex = e.target.dataset.pfIndex;
+    const pIndex = e.target.dataset.pIndex;
+    const tdIndex = e.target.dataset.tdIndex;
+    projectFolders[pfIndex].projects[pIndex].todos.splice(tdIndex, 1, editedTodo);
+    removeTodoModal();
+    displayPriority();
+}
+
+function removeTodoPriority(e) {
+    const pfIndex = e.target.dataset.pfIndex;
+    const pIndex = e.target.dataset.pIndex;
+    const tdIndex = e.target.dataset.tdIndex;
+    projectFolders[pfIndex].projects[pIndex].todos.splice(tdIndex, 1);
+    displayFolders(projectFolders);
+    displayPriority();
+}
+
+function checkTodoPriority(e) {
+    const pfIndex = e.target.dataset.pfIndex;
+    const pIndex = e.target.dataset.pIndex;
+    const tdIndex = e.target.dataset.tdIndex;
+    projectFolders[pfIndex].projects[pIndex].todos[tdIndex].taskComplete = true;
+    projectFolders[pfIndex].projects[pIndex].todos[tdIndex].priority = false;
+    displayPriority();
+}
