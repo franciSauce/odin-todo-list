291db024d162a3acbf0b8a031a2ce37984ea1380
franciSauce <careyfrancisowino@gmail.com>
2025-05-21 11:17:25 +0300

    Setup the imports

---
+++ src/controllers/projects.js
@@ -0,0 +1,5 @@
+import { projectFolders } from "../index";
+import { Project } from "../factory-functions" ;
+import { displayFolders, displayProject } from "../dom/projects" ;
+import { removeProjectModal } from "../dom/modal";
+
