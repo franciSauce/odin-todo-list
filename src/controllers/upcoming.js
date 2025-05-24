ec3586cec470eb312d77135da9286e4ebd46a58f
franciSauce <careyfrancisowino@gmail.com>
2025-05-24 10:34:30 +0300

    Fix typo

--- src/controllers/upcoming.js
+++ src/controllers/upcoming.js
@@ -1,4 +1,4 @@
-import { projectFolders } from "../index ";
+import { projectFolders } from "../index";
 import { Todo } from "../factory-functions";
 import { removeTodoModal } from "../dom/modal";
 import { displayUpcoming } from "../dom/links";
