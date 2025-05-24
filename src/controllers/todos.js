4e3c7b8e9271065acb7127325ad8a81fc874ee1d
franciSauce <careyfrancisowino@gmail.com>
2025-05-24 09:56:50 +0300

    Setup the imports

---
+++ src/controllers/todos.js
@@ -0,0 +1,5 @@
+import { projectFolders } from "../index";
+import { Todo } from "../factory-functions";
+import { displayFolders, displayProject } from "../dom/projects";
+import { removeTodoModal} from "../dom/modal";
+
