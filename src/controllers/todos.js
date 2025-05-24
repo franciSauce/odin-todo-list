34cc66ad7c8328fe2f20e33efc56af3a56bb07a1
franciSauce <careyfrancisowino@gmail.com>
2025-05-24 10:02:04 +0300

    Add addTodo()

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -3,3 +3,27 @@ import { Todo } from "../factory-functions";
 import { displayFolders, displayProject } from "../dom/projects";
 import { removeTodoModal} from "../dom/modal";
 
+function addTodo(e) {
+    const taskName = document.querySelector("#task-name");
+    const dueDate = document.querySelector("#due-date");
+    const priority = document.querySelector("#priority");
+    const task = taskName.value;
+    const date = dueDate.value;
+    const newTodo = Todo(task, date);
+    if (!task || !date) {
+        alert("Please fill in all fields");
+        return;
+    }
+    if (priority.checked) {
+        newTodo.priority = true;
+    } else {
+        newTodo.priority = false;
+    }
+    const pfIndex = e.target.dataset.pfIndex;
+    const pIndex = e.target.dataset.pIndex;
+    projectFolders[pfIndex].projects[pIndex].todos.push(newTodo);
+    removeTodoModal();
+    displayProject(projectFolders[pfIndex].projects[pIndex], pfIndex, pIndex);
+    displayFolders(projectFolders);
+}
+
