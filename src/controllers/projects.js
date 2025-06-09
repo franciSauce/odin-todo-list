f616dac245d2933420bedae5207401d1feb9e509
franciSauce <careyfrancisowino@gmail.com>
2025-06-09 11:23:31 +0300

    Add numericIndex

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -4,6 +4,11 @@ import { displayFolders, displayProject } from "../dom/projects" ;
 import { removeProjectModal } from "../dom/modal";
 
 function addProject(e) {
+    console.log("Event target:", e.target);
+    console.log("Dataset:", e.target.dataset);
+    console.log("pfIndex value:", e.target.dataset.pfIndex);
+    console.log("projectFolders array:", projectFolders);
+
     const projectName = document.querySelector("#p-name");
     const projectDescription = document.querySelector("#p-desc");
     const project = projectName.value;
@@ -14,14 +19,20 @@ function addProject(e) {
     }
     const newProj = Project(project, description);
     const index = e.target.dataset.pfIndex;
-    if (projectFolders[index] === undefined) {
-        console.error("projectFolders[index] is undefined. Check the index or the projectFolders array.");
+
+     // Convert to number if it's a string
+    const numericIndex = parseInt(index, 10);
+    
+    if (isNaN(numericIndex) || projectFolders[numericIndex] === undefined) {
+        console.error("Invalid index:", index, "Numeric index:", numericIndex);
+        console.error("projectFolders length:", projectFolders.length);
         return;
     }
-    projectFolders[index].projects.push(newProj);
+
+    projectFolders[numericIndex].projects.push(newProj);
     removeProjectModal();
     displayFolders(projectFolders);
-    displayProject(newProj, index, projectFolders[index].projects.length - 1);
+    displayProject(newProj, numericIndex, projectFolders[numericIndex].projects.length - 1);
 }
 
 function editProject(e) {
